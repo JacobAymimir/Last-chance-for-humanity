b38824ee5183a618d2a98ec7d153dfd968980d35
jacob <j.aymimir@gmail.com>
2022-10-25 16:06:45 +0200

    first forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  form: FormGroup;
+  constructor(private formBuilder: FormBuilder) { 
+    this.form = this.formBuilder.group({
+      language: new FormControl('', Validators.compose([Validators.required])),
+    })
+  }
+ get f(){
+    return this.form.controls;
+  }
+
+  submit(){
+
+    console.log(this.form.value.language);    
+  }
+  change(e:any){
+      console.log(e.target.value);
+  }
   title = 'LastChanceForHumanity';
 }
